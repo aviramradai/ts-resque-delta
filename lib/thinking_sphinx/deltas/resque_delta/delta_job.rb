a89601e5e31b5b11c903d02cc9a3cf2d57f06c1c
Aviram Radai <aviram.radai@samanage.com>
2015-06-11 14:53:25 +0300

    need max_delta_update to be in scope

--- lib/thinking_sphinx/deltas/resque_delta/delta_job.rb
+++ lib/thinking_sphinx/deltas/resque_delta/delta_job.rb
@@ -37,9 +37,7 @@ class ThinkingSphinx::Deltas::ResqueDelta::DeltaJob
       else
         model_class.classify.constantize
       end
-      Replica.use do
-        max_delta_update = model_table.select("max(updated_at) as max_updated_at").where(["delta=?",1]).first.max_updated_at
-      end
+      max_delta_update = Replica.use { model_table.select("max(updated_at) as max_updated_at").where(["delta=?",1]).first.max_updated_at }
     end
 
     # Delta Index
